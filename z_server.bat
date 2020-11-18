7dd5f3e93b50ceb3b168be9e0ece71d82bd464d2
INeedBots <ineedbots@outlook.com>
2020-11-18 02:49:30 -0600

    bat

---
+++ z_server.bat
@@ -0,0 +1,1 @@
+start "" "%~dp0BlackOpsMP.exe" +set dedicated "2" +set sv_maxclients "32" +set sv_licensenum "0" +set sv_punkbuster "0" +set sv_ranked "2" +set net_port "28960" +set developer "1" +set fs_game "mods/mp_bots" +exec server.cfg +map mp_nuked
