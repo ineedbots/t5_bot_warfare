767a5a53ac3aa67d2922decd0ec8ac72ee416c1d
INeedBots <ineedbots@outlook.com>
2020-12-21 20:24:21 -0600

    clean up

--- z_server.bat
+++ z_server.bat
@@ -1,1 +1,1 @@
-start "" "%~dp0BlackOpsMP.exe" +set dedicated "2" +set sv_maxclients "32" +set sv_licensenum "0" +set sv_punkbuster "0" +set sv_ranked "2" +set net_port "28960" +set developer "1" +set fs_game "mods/mp_bots" +exec server.cfg +map mp_nuked
+start "" "%~dp0BlackOpsMP.exe" +set dedicated "2" +set sv_maxclients "32" +set sv_licensenum "0" +set sv_punkbuster "0" +set sv_ranked "2" +set net_port "28960" +set net_ip "192.168.1.136" +set developer "0" +set fs_game "" +exec server.cfg +map mp_nuked
