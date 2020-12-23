b7772c71b38d28e6b29bd6b9a3ce0d4b774e05e5
INeedBots <ineedbots@outlook.com>
2020-12-22 22:33:30 -0600

    gantilag

--- z_server.bat
+++ z_server.bat
@@ -1,1 +1,1 @@
-start "" "%~dp0BlackOpsMP.exe" +set dedicated "2" +set sv_maxclients "32" +set sv_licensenum "0" +set sv_punkbuster "0" +set sv_ranked "2" +set net_port "28960" +set net_ip "192.168.1.136" +set developer "0" +set fs_game "" +exec server.cfg +map mp_nuked
+start "" "%~dp0BlackOpsMP.exe" +set dedicated "2" +set sv_maxclients "32" +set sv_licensenum "0" +set sv_punkbuster "0" +set sv_ranked "2" +set net_port "28960" +set net_ip "192.168.1.136" +set developer "0" +set fs_game "" +set g_antilag "0" +exec server.cfg +map mp_nuked
