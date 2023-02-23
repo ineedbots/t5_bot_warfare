72a23a9d6db5bbc1c30ec99933f7066d62597d8d
MaliceGFS <125225012+MaliceGFS@users.noreply.github.com>
2023-02-22 19:59:20 -0600

    Updated server bat from rekt format to plutoniumt5

--- z_server.bat
+++ z_server.bat
@@ -1,24 +1,25 @@
 @echo off
-::RemoteCONtrol password, needed for most management tools like IW4MADMIN and B3. Do not skip if you installing IW4MADMIN.
-set rcon_password=
-::Name of the config file the server should use.
+::Paste the server key from https://platform.plutonium.pw/serverkeys here
+set key=placeServerKeyHere
+::Name of the config file the server should use. (default: dedicated.cfg)
 set cfg=server.cfg
 ::Name of the server shown in the title of the cmd window. This will NOT bet shown ingame.
-set name=RektT5 Bot Warfare
+set name=PlutoniumT5 Bot Warfare
 ::Port used by the server (default: 28960)
 set port=28967
-::What ip to bind too
-set ip=0.0.0.0
 ::Mod name (default "")
-set mod=
+set mod=""
+::Only change this when you don't want to keep the bat files in the game folder. MOST WON'T NEED TO EDIT THIS!
+set gamepath=%cd%
 
-title RektT5 MP - %name% - Server restarter
+title PlutoniumT5 MP - %name% - Server restarter
 echo Visit plutonium.pw / Join the Discord (a6JM2Tv) for NEWS and Updates!
-echo Server "%name%" will load "%cfg%" and listen on port "%port%" UDP with IP "%ip%"!
+echo Server "%name%" will load %cfg% and listen on port %port% UDP!
 echo To shut down the server close this window first!
 echo (%date%)  -  (%time%) %name% server start.
 
+cd /D %LOCALAPPDATA%\Plutonium
 :server
-start /wait /abovenormal "%name%" "%~dp0rekt\t5m.exe" -console +set dedicated "2" +set net_ip "%ip%" +set net_port "%port%" +set rcon_password "%rcon_password%" +set fs_game "%mod%" +exec "%cfg%"
+start /wait /abovenormal bin\plutonium-bootstrapper-win32.exe t5mp "%gamepath%" -dedicated +set key %key% +set fs_game %mod% +sv_config %cfg% +set net_port %port% +start_map_rotate
 echo (%date%)  -  (%time%) WARNING: %name% server closed or dropped... server restarts.
 goto server
